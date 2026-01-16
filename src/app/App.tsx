7cdab6487ef9143bb72829935b5cb9901f7d1a95
Robert Tyssen <rob.tyssen@gmail.com>
2026-01-16 14:38:00 -0500

    chore(deps): added additional mantine packages
    added @mantine/dates for date inputs;
    added @mantine/form for form inputs and validators;
    added @mantine/notifications for notification prompts

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,15 +1,23 @@
 import { MantineProvider } from "@mantine/core";
-import "@mantine/core/styles.css";
+import { ModalsProvider } from "@mantine/modals";
+import { Notifications } from "@mantine/notifications";
 import { RouterProvider } from "@tanstack/react-router";
 import { DependenciesProvider } from "./di/DependenciesProvider";
 import { router } from "./routes";
+
+import "@mantine/core/styles.css";
+import "@mantine/dates/styles.css";
+import "@mantine/notifications/styles.css";
 
 const App = () => {
   return (
     <>
       <DependenciesProvider>
         <MantineProvider>
-          <RouterProvider router={router} />
+          <ModalsProvider>
+            <Notifications />
+            <RouterProvider router={router} />
+          </ModalsProvider>
         </MantineProvider>
       </DependenciesProvider>
     </>
