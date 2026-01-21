5141812eeef984b9053cb97768d6ea2a5711f301
Robert Tyssen <rob.tyssen@gmail.com>
2026-01-20 20:21:45 -0500

    fix: fixed incorrect errors on update-todo

--- src/domain/usecases/update-todo.ts
+++ src/domain/usecases/update-todo.ts
@@ -1,15 +1,16 @@
+import { errorNotFound, errorTitleEmpty } from "../errors";
 import type { Todo } from "../todo";
 import type { TodoRepository } from "../todo-repository";
 
 export const updateTodo = async (repo: TodoRepository, t: Todo) => {
   // Validate that the title is not empty
   if (!t.title.trim()) {
-    throw new Error("Title cannot be empty");
+    throw new Error(errorTitleEmpty);
   }
 
   const result = await repo.update(t);
   if (!result) {
-    throw new Error("To-Do not found");
+    throw new Error(errorNotFound);
   }
 
   return result;
